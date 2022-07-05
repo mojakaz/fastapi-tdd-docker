a64530baf55f4100116ac17ee6f9f09521693c96
Kazuki Takemoto <kaztk@outlook.jp>
2022-07-05 12:17:08 +0900

    Mid RESTful Routes

--- project/app/main.py
+++ project/app/main.py
@@ -1,24 +1,30 @@
-import os
+import logging
 
-from fastapi import Depends, FastAPI
-from tortoise.contrib.fastapi import register_tortoise
+from fastapi import FastAPI
+from app.db import init_db
+from app.api import ping, summaries
 
-from app.config import Settings, get_settings
 
-app = FastAPI()
+log = logging.getLogger("uvicorn")
 
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
