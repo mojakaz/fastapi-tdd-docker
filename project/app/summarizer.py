47d662a0b84d3f65f61402c5b1818d4274cca58b
Kazuki Takemoto <kaztk@outlook.jp>
2022-07-15 00:35:44 +0900

    Remove project/env directory and start using pipenv

---
+++ project/app/summarizer.py
@@ -0,0 +1,9 @@
+from newspaper import Article
+
+
+def generate_summary(url: str) -> str:
+    article = Article(url)
+    article.download()
+    article.parse()
+article.nlp()
+    return article.summary
